972abcbc63199a2016c02cf2301900ca856534b3
mariiusw <marius.wetterlin@hotmail.com>
2016-02-17 12:26:01 +0100

    struktur rydding

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>test</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>test</title>
+    </head>
 
-<body>
-<h1>Handleliste</h1>
-<ul>
-	<li>Melk</li>
-    <li>Banan</li>
-    <li>Is</li>
-    <li>Epler</li>
-</ul>
+    <body>
+        <h1>Handleliste</h1>
+        <ul>
+            <li>Melk</li>
+            <li>Banan</li>
+            <li>Is</li>
+            <li>Epler</li>
+        </ul>
 
-    <p>hello world!</p>
-    <p>yolo</p>
-    <h1>feilmelding</h1>
-</body>
+    </body>
 </html>
