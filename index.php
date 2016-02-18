462c307fd00c61c7e3baf40963b826cde041cabb
Bror <brorbrurberg@me.com>
2016-02-18 11:48:19 +0100

    wordpress funksjoner

--- index.php
+++ index.php
@@ -11,14 +11,30 @@
 
 
     <body>
-        <!-- navbar -->
 
-        <!-- navbar -->
+		<?php if ( have_posts() ) : ?>
 
+                    <?php while ( have_posts() ) : the_post(); ?>
+                            <?php get_template_part( 'content'); ?>
+                    <?php endwhile; ?>
 
-        <!-- innhold -->
+                    <!-- Pagination -->
+                    <?php
+                    global $wp_query;
 
-        <!-- innhold -->
+                    $big = 999999999; // need an unlikely integer
+                    echo '<ul class="pagination">';
+                    echo paginate_links( array(
+                        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                        'format' => '?paged=%#%',
+                        'current' => max( 1, get_query_var('paged') ),
+                        'total' => $wp_query->max_num_pages
+                    ) );
+                    echo '</ul>';
+                    ?>
 
-    </body>
-</html>
+                	<?php else : ?>
+                    <?php get_template_part( 'content', 'none' ); ?>
+        <?php endif; ?>
+
+<?php get_footer(); ?>
