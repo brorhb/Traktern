cabf03524703ae2bf7305c89b459c22d2b2cddeb
mariiusw <marius.wetterlin@hotmail.com>
2016-03-15 13:23:18 +0100

    åpningstider

--- laurels/front-page.php
+++ laurels/front-page.php
@@ -42,44 +42,30 @@ get_header(); ?>
 <div class="pattern2">
     <div class="webpage-container container">
         <div class="col-md-9">
-                <!-- TRE SISTE POSTER -->
-                <!-- TRE SISTE POSTER -->
-                <div class="ramme">
-                    <div class="webpage-container container">
-                            <?php
-                            $custom_query = new WP_Query(array(
-                                'posts_per_page'    => 3,
-                                'cat'     => '22',
-                                'paged'             => 1,
-                            )); ?>
-
-                            <?php
-                            if ($custom_query->have_posts()) :
-                                while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
-                                    <div class="col-md-3">
-                            <!-- NEWS TITLE -->
-                                        <div class="news-single-title">
-                                            <h2><?php the_title(); ?></h2>
-                                        </div>
-                                        <div class="indre_ramme">
-                                            <?php the_excerpt($more); ?>
-                                        </br>
-                                            <hr class="visible-xs visible-sm">
-                                        </div>
-                                        </div>
-                                <?php
-                                endwhile;
-                            else :
-                                for ($i = 0; $i < 8; $i++) :
-                                    echo '';
-                                endfor;
-                            endif;
-
-                            wp_reset_postdata(); ?>
-                    </div>
-                </div>
-                <!-- TRE SISTE POSTER -->
-                <!-- TRE SISTE POSTER -->
+            <h3>Åpningstider</h3>
+            <ul class="tabs">
+                <li rel="storgata" class="active">Storgata</li>
+                <li rel="sjøsiden">Sjøsiden</li>
+            </ul>
+            
+            <div id="storgata" class="panel active">
+                <ul>
+                    <li>Mandag – fredag: 10-20</li>
+                    <li>Lørdag: 10-18</li>
+                    <li>Søndag: stengt</li>
+                </ul>
+            </div>
+            
+            <div id="sjøsiden" class="panel">
+                <ul>
+                    <li>mandag 08-18</li>
+                    <li>tirsdag-torsdag 08-20</li>
+                    <li>fredag 08-18</li>
+                    <li>lørdag 10-18</li>
+                    <li>søndag 10-18</li>
+                </ul>
+            </div>
+            
         </div>
         <div class="col-md-3 hidden-xs">
             <div class="kaffe"><img src="http://136147-www.web.tornado-node.net/wp-content/themes/laurels/images/kaffe.png" alt="kaffekopp">
