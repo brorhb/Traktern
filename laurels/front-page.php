9973266d9be97b156015f9143c273e7b5c8d4c60
Bror <brorbrurberg@me.com>
2016-03-15 13:03:53 +0100

    små fiks

--- laurels/front-page.php
+++ laurels/front-page.php
@@ -31,62 +31,77 @@ get_header(); ?>
                     wp_reset_postdata();
                     ?>
             </div>
-            <div class="breadcrumb site-breadcumb">
-				<?php if (function_exists('laurels_custom_breadcrumbs')) laurels_custom_breadcrumbs(); ?>
-            </div>
+
             </div>
     	</div>
     </div>
-    <div class="pattern2">
-        <div class="container webpage-container">
-        	<article class="blog-article">
-
-
-
-    	<div id="post-<?php the_ID(); ?>" <?php post_class("col-md-9 col-sm-8 blog-page"); ?>>
-          <?php while ( have_posts() ) : the_post(); ?>
-          <?php $laurels_image = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) ); ?>
-                    <div class="blog">
-
-                        <div class="blog-data">
-                            </div>
-                            <div class="blog-info">
-                            </div>
-
-                            <div class="blog-rightsidebar-img">
-    					<?php if(!empty($laurels_image)) { ?><img src="<?php echo esc_url($laurels_image); ?>" class="img-responsive" alt="<?php the_title(); ?>" /><?php } ?>
-                        </div>
-
-                            <div class="blog-content">
-                                <?php the_content();
-    									wp_link_pages( array(
-    										'before' => '<div class="page-links">' . __( 'Pages:', 'besty' ),
-    										'after' => '</div>',
-    									) ); ?>
-                            </div>
-                        </div>
-        </div>
-        <div class="col-md-3 hidden-xs">
-            <div class="kaffe"><img src="http://136147-www.web.tornado-node.net/wp-content/themes/laurels/images/kaffe.png" alt="kaffekopp"></div>
-        </div>
-              <?php endwhile; ?>
-                    <div class="comments">
-    					 <?php comments_template( '', true ); ?>
-                    </div>
-        </div>
-    </div>
-             <!-- get_sidebar(); -->
     	</article>
 
+<!-- ÅPNINGSTIDER -->
+<!-- ÅPNINGSTIDER -->
+<div class="pattern2">
+    <div class="webpage-container container">
+        <div class="col-md-9">
+                <!-- TRE SISTE POSTER -->
+                <!-- TRE SISTE POSTER -->
+                <div class="ramme">
+                    <div class="webpage-container container">
+                            <?php
+                            $custom_query = new WP_Query(array(
+                                'posts_per_page'    => 3,
+                                'cat'     => '22',
+                                'paged'             => 1,
+                            )); ?>
+
+                            <?php
+                            if ($custom_query->have_posts()) :
+                                while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
+                                    <div class="col-md-3">
+                            <!-- NEWS TITLE -->
+                                        <div class="news-single-title">
+                                            <h2><?php the_title(); ?></h2>
+                                        </div>
+                                        <div class="indre_ramme">
+                                            <?php the_excerpt($more); ?>
+                                        </br>
+                                            <hr class="visible-xs visible-sm">
+                                        </div>
+                                        </div>
+                                <?php
+                                endwhile;
+                            else :
+                                for ($i = 0; $i < 8; $i++) :
+                                    echo '';
+                                endfor;
+                            endif;
+
+                            wp_reset_postdata(); ?>
+                    </div>
+                </div>
+                <!-- TRE SISTE POSTER -->
+                <!-- TRE SISTE POSTER -->
+        </div>
+        <div class="col-md-3 hidden-xs">
+            <div class="kaffe"><img src="http://136147-www.web.tornado-node.net/wp-content/themes/laurels/images/kaffe.png" alt="kaffekopp">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ÅPNINGSTIDER -->
+<!-- ÅPNINGSTIDER -->
+
+
 
 <!-- TRE SISTE POSTER -->
 <!-- TRE SISTE POSTER -->
-<div class="ramme">
+<div class="ramme hidden-xs">
     <div class="webpage-container container">
+        <h1>Tre siste blogg innlegg</h1>
+        <hr>
             <?php
             $custom_query = new WP_Query(array(
                 'posts_per_page'    => 3,
-                'category_name'     => 'Nyheter',
+                'cat'     => '3',
                 'paged'             => 1,
             )); ?>
 
@@ -96,11 +111,10 @@ get_header(); ?>
                     <div class="col-md-4">
             <!-- NEWS TITLE -->
                         <div class="news-single-title">
-                            <h2><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h2>
+                            <h2><?php the_title(); ?></h2>
                         </div>
                         <div class="indre_ramme">
                             <?php the_excerpt($more); ?>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-default pull-right" role="button">Les mer</a>
                         </br>
                             <hr class="visible-xs visible-sm">
                         </div>
@@ -108,8 +122,8 @@ get_header(); ?>
                 <?php
                 endwhile;
             else :
-                for ($i = 0; $i < 3; $i++) :
-                    echo '<div class="news-single" style="height: 250px;"><p>Det er for øyeblikket ingen nyheter</p></div>';
+                for ($i = 0; $i < 8; $i++) :
+                    echo '';
                 endfor;
             endif;
 
